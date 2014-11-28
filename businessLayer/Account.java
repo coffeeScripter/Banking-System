8e3a519d043b858efabd2b9689acb8a360b4a867
Howdy52 <noahpeterham@gmail.com>
2014-11-28 17:44:19 -0500

    Added an object to represent an bank Account

---
+++ businessLayer/Account.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package businessLayer;
+
+import java.util.*;
+
+/**
+ * @author Noah
+ *
+ */
+public class Account {
+	
+	String name;
+	int number;
+	double balance;
+	ArrayList transHist;
+	/**
+	 * represnts an account having a 
+	 *  name,
+	 *  number, 
+	 *  balance, 
+	 *  and transaction history
+	 */
+	public Account() {
+		// TODO Auto-generated constructor stub
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public ArrayList getTransHist() {
+		return transHist;
+	}
+	public void setTransHist(ArrayList transHist) {
+		this.transHist = transHist;
+	}
+	
+
+}
