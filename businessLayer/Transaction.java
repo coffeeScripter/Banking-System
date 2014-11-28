c532d6b32f9338138cd63e110819a5cd22d8b768
Howdy52 <noahpeterham@gmail.com>
2014-11-28 17:56:20 -0500

    Added a transaction class

---
+++ businessLayer/Transaction.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package businessLayer;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * @author Noah
+ *
+ */
+public class Transaction {
+	SimpleDateFormat date;
+	char type;
+	String location;
+	double transAmt;
+	double newBalance;
+	
+	/**
+	 * 
+	 */
+	public Transaction() {
+		// TODO Auto-generated constructor stub
+	}
+	public Transaction(char type){
+		this.type = type;
+	}
+	/**
+	 * @param type
+	 * @param location
+	 * @param transAmt
+	 */
+	public Transaction(char type, String location, double transAmt) {
+		this.type = type;
+		this.location = location;
+		this.transAmt = transAmt;
+	}
+	public double computeBalance(){
+		return 20;
+	}
+	public boolean isValid(){
+		return false;
+	}
+	
+	// getters and setters
+	public SimpleDateFormat getDate() {
+		return date;
+	}
+
+	public char getType() {
+		return type;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public double getTransAmt() {
+		return transAmt;
+	}
+
+	public double getNewBalance() {
+		return newBalance;
+	}
+
+
+}
