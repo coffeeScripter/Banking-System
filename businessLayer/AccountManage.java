4f78be24ac6898b9e238013fa8d60ee72b8bd09e
brianduffy <bd8731@g.rit.edu>
2014-12-01 22:25:05 -0500

    business layer outlines

---
+++ businessLayer/AccountManage.java
@@ -0,0 +1,38 @@
+package businessLayer;
+/**
+ * 	@author Noah Peterham
+ * 	@author Brian Duffy
+ * 	@author Jon Koch
+ */
+
+
+public class AccountManage {
+	int accNum;
+	double balance;
+	String custID;
+	double intRate;
+	String usrID;
+	String usrPwd;
+	Token t;
+	
+	
+	public AccountManage(int aNum){
+		// get account info
+	}
+	
+	public boolean deposit(int aNum, double amt){
+		// Verify is customers account or employee
+		// get balance
+		// do math
+		// Account.postBal()
+		return true;
+	}
+	public boolean withdrawal(int aNum, double amt){
+		// Verify is customers account or employee
+		// get balance
+		// do math
+		// Account.postBal()
+		return true;
+	}
+	
+}
---
+++ businessLayer/Adminstrition.java
@@ -0,0 +1,48 @@
+package businessLayer;
+/**
+ * 	@author Noah Peterham
+ * 	@author Brian Duffy
+ * 	@author Jon Koch
+ */
+
+
+public class Adminstrition {
+	
+	
+	public Adminstrition(){
+		
+	}
+	
+	public int addPeople(){
+		int pID = 0;
+		//people.newPeople()
+		return pID; // new people id
+	}
+	public void modPeople(){
+		// people.postPeople()
+	}
+	public int addAccount(){
+		int aID = 0;
+		//people.newAccount()
+		return aID; // new Account num
+	}
+	public void modAccount(){
+		// people.postAccount()
+	}
+	public int addEmployee(){
+		int aID = 0;
+		//people.newEmployee()
+		return aID; // new Employee num
+	}
+	public void modEmployee(){
+		// people.postEmployee()
+	}
+	public int addCustomer(){
+		int aID = 0;
+		//people.newCustomer()
+		return aID; // new Customer num
+	}
+	public void modCustomer(){
+		// people.postCustomer()
+	}
+}
